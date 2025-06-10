b005ed05ab8c73b367a10bb07e68a8135b52a75a
Ade Suryani_2410432028 <adesuryani0018@gmail.com>
2025-06-10 21:13:50 +0700

    2410432028_Ade Suryani_Tugas_Modul_9_shift 2_pratikum_ADP.py

---
+++ 2410432028_Ade Suryani_Tugas_Modul_9_shift 2_pratikum_ADP.py
@@ -0,0 +1,34 @@
+import time
+from os import system
+from termcolor import colored, cprint
+
+
+system('cls')
+
+def kue():
+    kue = [
+        "    *   *  .  .  .       *   ",
+        "  *       _|__|__|_  *       ",
+        "     *  _|'''''''''|_   *    ",
+        " *    _|||||||||||||||_   *  ",
+        "     |~~~~H-A-P-P-Y~~~~|     ",
+        "  *  |-B-I-R-T-H-D-A-Y-| *   ",
+        "     |~0~0~0~0~0~0~0~0~|  *  ",
+        "     ~~~~~~~~~~~~~~~~~~~     "
+    ]
+
+    
+    for baris in kue:
+        cprint(baris, "white", "on_red")
+        time.sleep(0.3)
+
+def ucapan() :
+    time.sleep(0.5)
+    cprint("    Selamat Ulang Tahun !    ", "red", "on_white")
+    time.sleep(1)
+    cprint("Semoga kamu bahagia selalu :)","red", "on_white")
+
+
+kue()
+time.sleep(0.2)
+ucapan()
